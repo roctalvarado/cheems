fcb224a4d9eea53ef4f7e5636e37314976c6f302
Carlotta <juan.rubio262132@potros.itson.edu.mx>
2025-12-09 17:57:10 -0700

    return db config

--- persistence/db.py
+++ persistence/db.py
@@ -1,7 +1,7 @@
-import pymysql 
+import pymysql as mysql
 
 def get_connection():
-    return pymysql.connect(
+    return mysql.connect(
         host='127.0.0.1', # o "localhost"
         port=3306, # Default = 3306
         user='root',
